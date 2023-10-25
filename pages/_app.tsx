df6961a42e4370d226d36352823299ada8ffa714
Tomas Rawski <rtomas@gmail.com>
2023-10-25 17:08:49 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([polygonMumbai], [publicProvider()]);
+
+const config = createConfig({
+    autoConnect: true,
+    connectors: [new InjectedConnector({ chains })],
+    publicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <WagmiConfig config={config}>
+            <Component {...pageProps} />
+        </WagmiConfig>
+    );
 }
